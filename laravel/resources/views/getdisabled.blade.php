b001dd7dca11d36290b5a0c7b55d9ba7302885fe
Hannu Oksman <35224428+Szeretni@users.noreply.github.com>
2018-12-01 18:41:47 +0200

    Update getdisabled.blade.php

--- laravel/resources/views/getdisabled.blade.php
+++ laravel/resources/views/getdisabled.blade.php
@@ -1,3 +1,5 @@
 <?php
 
+<!-- Ilmoitus selainkäyttäjälle, että tämä resurssi on otettu pois käytöstä HTTP GET-verbeille. -->
+
 echo "Get requests for authentication are disabled. Please use post requests.";
