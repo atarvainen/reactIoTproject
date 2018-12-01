a9418b1c4fd2d598b01e674c4447cf85592c4d58
Hannu Oksman <35224428+Szeretni@users.noreply.github.com>
2018-12-01 17:18:04 +0200

    Update RuuvitagsUser.php

--- laravel/app/RuuvitagsUser.php
+++ laravel/app/RuuvitagsUser.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class RuuvitagsUser extends Model
 {
-    //
+    //Ei itse toteutettua toiminnallisuutta, mutta tätä modelia voi silti käyttää. Modelista saa esim. userid:n ulos
 }
